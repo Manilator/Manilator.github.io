c0ff68000536242b8ee45a701552e7afd156b172
Miguel Solino <manilator777@gmail.com>
2020-07-08 15:09:37 +0100

    Base

---
+++ portfolio/src/components/AnimatedBackground.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="animated-background">
+      <div class="forms">
+          <img src="../assets/x.svg" alt="">
+          <img src="../assets/x.svg" alt="">
+          <img src="../assets/x.svg" alt="">
+          <img src="../assets/x.svg" alt="">
+          <img src="../assets/x.svg" alt="">
+          <img src="../assets/x.svg" alt="">
+          <img src="../assets/x.svg" alt="">
+          <img src="../assets/x.svg" alt="">
+          <img src="../assets/x.svg" alt="">
+          <img src="../assets/x.svg" alt="">
+          <img src="../assets/x.svg" alt="">
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AnimatedBackground',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.animated-background {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+}
+
+.forms img {
+    position: absolute;
+    height: 30px;
+    background-color: transparent;
+    fill: white;
+    filter: invert(0);
+}
+
+.forms img:nth-child(1) {
+    top: 12%;
+    left: 42%;
+    animation: animate 10s linear infinite;
+}
+
+.forms img:nth-child(2) {
+    top: 70%;
+    left: 50%;
+    animation: animate 7s linear infinite;
+}
+
+.forms img:nth-child(3) {
+    top: 13%;
+    left: 6%;
+    animation: animate 9s linear infinite;
+}
+
+.forms img:nth-child(4) {
+    top: 67%;
+    left: 60%;
+    animation: animate 10s linear infinite;
+}
+
+.forms img:nth-child(5) {
+    top: 67%;
+    left: 10%;
+    animation: animate 6s linear infinite;
+}
+
+.forms img:nth-child(6) {
+    top: 80%;
+    left: 70%;
+    animation: animate 11s linear infinite;
+}
+
+.forms img:nth-child(7) {
+    top: 60%;
+    left: 80%;
+    animation: animate 12s linear infinite;
+}
+
+.forms img:nth-child(8) {
+    top: 32%;
+    left: 25%;
+    animation: animate 14s linear infinite;
+}
+
+.forms img:nth-child(9) {
+    top: 90%;
+    left: 25%;
+    animation: animate 9s linear infinite;
+}
+
+.forms img:nth-child(10) {
+    top: 20%;
+    left: 80%;
+    animation: animate 5s linear infinite;
+}
+
+.forms img:nth-child(11) {
+    top: 90%;
+    left: 90%;
+    animation: animate 4s linear infinite;
+}
+
+@keyframes animate{
+    0%{
+        transform: scale(0) translate(0) rotate(0);
+        opacity: 1;
+    }
+    100%{
+        transform: scale(1.3) translateY(-90px) rotate(360deg);
+        opacity: 0;
+    }
+}
+/*
+@media (prefers-color-scheme: dark) {
+.forms img {
+    filter: invert(100);
+  }
+}
+*/
+</style>
---
+++ portfolio/src/components/NavBar.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="principal">
+    <div id="dev">Designer & Front-end Developer</div>
+    <a id="logo">
+      <img src="../assets/logo1.png" height="200px">
+    </a>
+    <div id="name">Miguel Solino</div>
+    <div id="desc">Bachelors Degree in Computer Engineering @ University of Minho</div>
+    <div id="navbar">
+      <a href="" id="navbar-item">
+        <img src="../assets/twitter.png" alt="Twitter" height="40px">
+      </a>
+      <a href="" id="navbar-item">
+        <img src="../assets/github.png" alt="Github" height="40px">
+      </a>
+      <a href="" id="navbar-item">
+        <img src="../assets/linkedin.png" alt="Linkedin" height="40px">
+      </a>
+      <a href="" id="navbar-item">
+        <img src="../assets/email.png" alt="Email" height="40px">
+      </a>
+      <div id="madeby">© Miguel Solino</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {
+    logo: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.principal {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: transparent;
+}
+
+#logo {
+  margin-bottom: 50px;
+}
+
+#name {
+  padding: 10px;
+  padding-bottom: 0px;
+  font-size: 30px;
+  font-weight: bold;
+  color: rgb(41, 40, 44);
+}
+
+#dev {
+  margin: 20px;
+  margin-bottom: 40px;
+  font-size: 40px;
+  font-weight: bold;
+  color: rgb(41, 40, 44);
+}
+
+
+
+#desc {
+  margin: auto;
+  width: 60%;
+  text-align: center;
+  color: rgb(29, 29, 37);
+}
+
+#navbar {
+  margin: 20px;
+}
+ 
+#navbar-item img {
+  margin: 10px;
+  background-color: transparent;
+  transition: transform .1s ease;
+}
+
+#navbar-item:hover img{
+  opacity: 80%;
+  -ms-transform: scale(1.5); /* IE 9 */
+  -webkit-transform: scale(1.5); /* Safari 3-8 */
+  transform: scale(1.5); 
+  transition: transform .1s ease;
+}
+
+#madeby {
+  color: grey;
+  margin: 10px;
+}
+</style>
